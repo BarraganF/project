59c570111e83b52c43f5642c58a0190f7daef941
BarraganF <barraganjavier0114@gmail.com>
2025-08-26 17:04:15 -0500

    header-1

---
+++ mi-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header  className='caja-header'>
+        <div className='title'>
+            <h1>WayPoint</h1>
+        </div>
+
+    </header>
+  )
+}
